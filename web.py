a2586fbf7f1757d59f858a525b84613224f55878
railla01 <3mfkr.modular@gmail.com>
2025-05-08 10:43:08 -0300

    web.py

--- web.py
+++ web.py
@@ -1,5 +1,19 @@
 from bot import app
+from threading import Thread
+from flask import Flask
+
+# 🔁 Bot em paralelo
+def iniciar_bot():
+    print("🚀 DRI rodando via Web Service no Render!")
+    app.run()
+
+# 🌐 Flask para simular servidor web
+web_app = Flask("DRI-Flask")
+
+@web_app.route("/")
+def home():
+    return "DRI Bot está online! 🤖"
 
 if __name__ == "__main__":
-    print("🚀 DRI Web Service Rodando como Telegram Bot!")
-    app.run()
+    Thread(target=iniciar_bot).start()
+    web_app.run(host="0.0.0.0", port=10000)
